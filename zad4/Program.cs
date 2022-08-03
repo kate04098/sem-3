5beeef41c0264e272044bf09033de21bb11e5cb5
Ekaterina <artyuhovae@mail.ru>
2022-08-04 01:55:22 +0300

    add zad4

---
+++ zad4/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+int number = int.Parse(inputLineN);
+
+string lineN = " ";
+string lineNN = string.Empty;
+string s = 1;
+while (s <= numberN)
+{
+lineN = lineN + s + " ";
+lineNN = linen + (s * s).ToString() + "";
+s++;
+}
+Console.WriteLine(lineN);
+Console.WriteLine(lineNN);
+}
