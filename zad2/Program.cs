50f28e744d9655fb99db6b74b9f5b62507d6bcad
Ekaterina <artyuhovae@mail.ru>
2022-08-04 01:45:30 +0300

    add zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,16 @@
+﻿void printAnswer(int number)
+{
+if(number==1) Console.WriteLine("Дoпустимо: x>0, y>0");
+if(number==2) Console.WriteLine("Дoпустимо: x<0, y>0");
+if(number==3) Console.WriteLine("Дoпустимо: x<0, y<0");
+if(number==4) Console.WriteLine("Дoпустимо: x>0, y<0");
+}
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null);
+{
+
+int inputLine = int.Parse(inputLine);
+
+printAnswer(inputNumber);
+}
